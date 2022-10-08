7ea4429340e9c1150aa410a83c6316dfe8e237f9
escalopa <ahmad.helaly.dev@gmail.com>
2022-10-08 16:25:07 +0300

    add source to zsh-installer

--- zsh-install.sh
+++ zsh-install.sh
@@ -11,6 +11,8 @@ then
 else
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -y)"
 
+  source ~/.zshrc
+
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
   git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
