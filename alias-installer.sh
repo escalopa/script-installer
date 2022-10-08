afab96874437559cd33a6510fb29581ba8e029b7
escalopa <ahmad.helaly.dev@gmail.com>
2022-10-08 15:31:39 +0300

    add install scripts

---
+++ alias-installer.sh
@@ -0,0 +1,33 @@
+#!/usr/bin
+
+set -e
+
+touch ~/.bash_aliases
+
+# shellcheck disable=SC1078
+echo "#
+
+git
+alias gs='git status'
+alias ga='git add'
+alias gc='git commit'
+alias gb='git branch'
+alias gco='git checkout'
+alias gps='git push'
+alias gpl='git pull'
+
+# hardhat
+
+# go
+alias gr='go run'
+
+# sys navigation
+alias ..='cd ..'
+
+# other
+alias bat='upower -i /org/freedesktop/UPower/devices/battery_BAT0| grep -E \"state|to\ full|percentage\"'
+alias lock='gnome-screensaver-command -l'
+" >> ~/.bash_aliases
+
+# shellcheck disable=SC1090
+source ~/.bashrc
---
+++ zsh-install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin
+
+set -e
+
+# shellcheck disable=SC2034
+NEW_LINE_TAB='\n\t'
+
+if [ "$(id -u)" != "0" ]; then
+  echo "This script requires sudo access to install"
+  sudo echo
+fi
+
+sudo apt install zsh -y
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -y)"
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+sed -i "s/robbyrussell/strug/" ~/.zshrc
+sed -i "s/plugins=(git)/plugins=($(NEW_LINE_TAB)git$(NEW_LINE_TAB)zsh-syntax-highlighting$(NEW_LINE_TAB)zsh-autosuggestions\n)/" ~/.zshrc
+
+# shellcheck disable=SC1090
+source ~/.shrc
+
+
+
