5d7ec8221c5598b7f60112b59dcbfc4e73cc9477
escalopa <ahmad.helaly.dev@gmail.com>
2022-10-08 18:23:20 +0300

    remove git check

--- zsh-install.sh
+++ zsh-install.sh
@@ -7,26 +7,22 @@ NEW_LINE_TAB='\n\t'
 # shellcheck disable=SC2088
 OH_MY_ZSH="~/.oh-my-zsh"
 
-if ! command -v git &>/dev/null; then
-  echo "git command could not be found"
-else
-  [ -d "$OH_MY_ZSH" ] && rm -rf "$OH_MY_ZSH"
-  sudo apt install zsh -y
-  curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
+[ -d "$OH_MY_ZSH" ] && rm -rf "$OH_MY_ZSH"
+sudo apt install zsh -y
+curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
 
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-  git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
 
-  sed -i "s/robbyrussell/strug/" ~/.zshrc
+sed -i "s/robbyrussell/strug/" ~/.zshrc
 
-  # shellcheck disable=SC2027
-  sed -i "s/plugins=(git)/plugins=("$NEW_LINE_TAB"git"$NEW_LINE_TAB"zsh-syntax-highlighting"$NEW_LINE_TAB"zsh-autosuggestions\n)/" ~/.zshrc
+# shellcheck disable=SC2027
+sed -i "s/plugins=(git)/plugins=("$NEW_LINE_TAB"git"$NEW_LINE_TAB"zsh-syntax-highlighting"$NEW_LINE_TAB"zsh-autosuggestions\n)/" ~/.zshrc
 
-  # shellcheck disable=SC1090
-  source ~/.zshrc
+# shellcheck disable=SC1090
+source ~/.zshrc
 
-  # shellcheck disable=SC2046
-  chsh -s $(which zsh)
-fi
+# shellcheck disable=SC2046
+chsh -s $(which zsh)
 
 set -ex
