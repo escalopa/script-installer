27f11304e73970ab94b4ee5301a90cdc025c74cf
Ahmad Helaly <58439278+escalopa@users.noreply.github.com>
2022-10-18 02:35:41 +0300

    added vbox install script

---
+++ scripts/vitrual-box.sh
@@ -0,0 +1,13 @@
+#!/usr/bin
+
+sudo echo "running install script"
+
+curl -fsSL https://www.virtualbox.org/download/oracle_vbox_2016.asc|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/oracle_vbox_2016.gpg
+curl -fsSL https://www.virtualbox.org/download/oracle_vbox.asc|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/oracle_vbox.gpg
+
+echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian bullseye contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+sudo apt update
+sudo apt install -y dkms
+
+sudo apt install -y virtualbox virtualbox-ext-pack
+
