e0794f3a7c90fdfdb38aaf390d367e59397eb689
Ahmad Helaly <58439278+escalopa@users.noreply.github.com>
2022-10-18 01:51:25 +0300

    added new scripts

---
+++ scripts/docker.sh
@@ -0,0 +1,17 @@
+#!/usr/bin
+
+sudo echo "running install script"
+
+sudo apt update
+sudo apt install -y docker.io
+sudo systemctl enable docker --now
+
+sudo usermod -aG docker $USER
+printf '%s\n' "deb https://download.docker.com/linux/debian bullseye stable" |
+  sudo tee /etc/apt/sources.list.d/docker-ce.list
+
+curl -fsSL https://download.docker.com/linux/debian/gpg |
+  sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-ce-archive-keyring.gpg
+
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
