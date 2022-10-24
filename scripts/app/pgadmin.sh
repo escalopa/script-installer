e801cff8cb73c4483cc6dfc606a5247021f3bc60
Ahmad Helaly <58439278+escalopa@users.noreply.github.com>
2022-10-24 12:10:59 +0300

    Update pgadmin.sh

--- scripts/app/pgadmin.sh
+++ scripts/app/pgadmin.sh
@@ -4,10 +4,11 @@ sudo echo "running install script"
 
 sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
 
-DEVICE_NAME = $(lsb_release -cs)
+DEVICE_NAME=$(lsb_release -cs)
 
-if DEVICE_NAME == 'kali-rolling';
-  DEVICE_NAME = bullseye
+if [ "$DEVICE_NAME" = "kali-rolling" ]
+then
+  DEVICE_NAME=bullseye
 fi
 
 sudo echo “deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$DEVICE_NAME/ pgadmin4 main” > /etc/apt/sources.list.d/pgadmin4.list
