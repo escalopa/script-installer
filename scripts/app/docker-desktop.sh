b134a94456360bb59dc143555149facff01062e7
Ahmad Helaly <58439278+escalopa@users.noreply.github.com>
2022-10-24 11:50:41 +0300

    Update docker-desktop.sh

--- scripts/app/docker-desktop.sh
+++ scripts/app/docker-desktop.sh
@@ -1,4 +1,7 @@
+#!/usr/bin
 
- sudo apt-get update
+sudo apt-get update
 
-curl "https://desktop.docker.com/linux/main/amd64/docker-desktop-4.12.0-amd64.deb?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64" --output docker-desktop.deb && sudo apt install docker-desktop.deb
+curl "https://desktop.docker.com/linux/main/amd64/docker-desktop-4.12.0-amd64.deb?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64" --output docker-desktop.deb 
+sudo apt install ./docker-desktop.deb
+rm -rf ./docker-desktop.deb
