4a18edfd77107da146d47f6ed8c98e4eddde43d1
Ahmad Helaly <58439278+escalopa@users.noreply.github.com>
2022-11-08 04:15:49 +0300

    Create frr.sh

---
+++ scripts/frr.sh
@@ -0,0 +1,12 @@
+sudo echo "Running script"
+
+# add GPG key
+curl -s https://deb.frrouting.org/frr/keys.asc | sudo apt-key add -
+
+# possible values for FRRVER: frr-6 frr-7 frr-8 frr-stable
+# frr-stable will be the latest official stable release
+FRRVER="frr-stable"
+echo deb https://deb.frrouting.org/frr $(lsb_release -s -c) $FRRVER | sudo tee -a /etc/apt/sources.list.d/frr.list
+
+# update and install FRR
+sudo apt update && sudo apt install frr frr-pythontools
