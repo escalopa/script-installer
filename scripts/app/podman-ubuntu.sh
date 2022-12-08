7d5c0ec25978762c555bb88a378decd782ac960d
Ahmad Helaly <58439278+escalopa@users.noreply.github.com>
2022-12-08 13:32:09 +0300

    Update podman-ubuntu.sh

--- scripts/app/podman-ubuntu.sh
+++ scripts/app/podman-ubuntu.sh
@@ -1,12 +1,10 @@
-sudo apt-get update -y
-apt-get install curl wget gnupg2 -y
-
-source /etc/os-release
-sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
-
-wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_${VERSION_ID}/Release.key -O- | apt-key add -
-
-apt-get update -qq -y
-apt-get -qq --yes install podman
-
-podman --version
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/xUbuntu_$(lsb_release -rs)/Release.key \
+  | gpg --dearmor \
+  | sudo tee /etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg > /dev/null
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg]\
+    https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/xUbuntu_$(lsb_release -rs)/ /" \
+  | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:unstable.list > /dev/null
+sudo apt-get update -qq
+sudo apt-get -qq -y install podman
