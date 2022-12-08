c387306fb17473228ad49bed3380cd0b1a026793
Ahmad Helaly <58439278+escalopa@users.noreply.github.com>
2022-12-08 14:21:21 +0300

    Update podman-ubuntu.sh

--- scripts/app/podman-ubuntu.sh
+++ scripts/app/podman-ubuntu.sh
@@ -3,18 +3,13 @@ set -e
 echo "Enter Password"
 sudo echo ""
 
-sudo -- sh -c '
-  echo "Installing Podman..."
+echo "Installing Podman..."
 
-  source /etc/os-release
-  sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
-  Next, download and add the GPG key with the following command:
+source /etc/os-release
+sudo -- sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
+sudo -- sh -c "wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_${VERSION_ID}/Release.key -O- | apt-key add -"
 
-  wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_${VERSION_ID}/Release.key -O- | apt-key add -
-  Next, update the repository and install Podman with the following command:
-
-  apt-get update -qq -y
-  apt-get -qq --yes install podman
-'
+sudo apt-get update -y
+sudo apt-get -y install podman
 
 podman --version
