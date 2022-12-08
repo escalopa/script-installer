d92c63c29f412ef8cd6b821cc672302d40091fe0
Ahmad Helaly <58439278+escalopa@users.noreply.github.com>
2022-12-08 13:29:27 +0300

    Create podman-ubuntu.sh

---
+++ scripts/app/podman-ubuntu.sh
@@ -0,0 +1,12 @@
+sudo apt-get update -y
+apt-get install curl wget gnupg2 -y
+
+source /etc/os-release
+sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
+
+wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_${VERSION_ID}/Release.key -O- | apt-key add -
+
+apt-get update -qq -y
+apt-get -qq --yes install podman
+
+podman --version
